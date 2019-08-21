789da82831a03931f4c5bf792822b56cee65f6a8
Serene Chongtrakul <serenelc@outlook.com>
2019-08-21 14:02:27 +0100

    Trying to figure out build a list

--- src/buildAList.java
+++ src/buildAList.java
@@ -1,5 +1,28 @@
 public class buildAList {
 
+    public static void main(String args) {
+        String str = "xyz";
+        String[] res = combination(str, 0);
+        //xy xz yz
+    }
+
+    private static String[] combination(String from, int startIndex) {
+        String[] temp = new String[from.length()];
+        int j = 0;
+
+        for (int k = 0; k < from.length(); k++) {
+            String first = String.valueOf(from.charAt(k));
+
+            for (int i = k + 1; i < from.length(); i++) {
+                String cont = first + from.charAt(i);
+                temp[j] = cont;
+                j++;
+            }
+        }
+
+        return temp;
+    }
+
     static String[] solve(String s) {
         char[] chars = s.toCharArray();
         int size = 0;
@@ -27,24 +50,23 @@ public class buildAList {
     start & end ---> Staring and Ending indexes in arr[]
     index  ---> Current index in data[]
     r ---> Size of a combination to be printed */
-    static void combinationUtil(int arr[], int data[], int start,
-                                int end, int index, int r)
-    {
+    static int[] combinationUtil(int arr[], int data[], int start,
+                                int end, int index, int r) {
         // Current combination is ready to be printed, print it
-        if (index == r)
-        {
-            for (int j=0; j<r; j++)
-                System.out.print(data[j]+" ");
-            System.out.println("");
-            return;
+        int[] res = new int[r];
+        if (index == r) {
+            for (int j = 0; j < r; j++) {
+                res[j] = data[j];
+                System.out.print(data[j] + " ");
+            }
+            return res;
         }
 
         // replace index with all possible elements. The condition
         // "end-i+1 >= r-index" makes sure that including one element
         // at index will make a combination with remaining elements
         // at remaining positions
-        for (int i=start; i<=end && end-i+1 >= r-index; i++)
-        {
+        for (int i=start; i<=end && end-i+1 >= r-index; i++) {
             data[index] = arr[i];
             combinationUtil(arr, data, i+1, end, index+1, r);
         }
@@ -55,7 +77,7 @@ public class buildAList {
     static void printCombination(int arr[], int n, int r)
     {
         // A temporary array to store all combination one by one
-        int data[]=new int[r];
+        int data[] = new int[r];
 
         // Print all combination using temprary array 'data[]'
         combinationUtil(arr, data, 0, n-1, 0, r);
